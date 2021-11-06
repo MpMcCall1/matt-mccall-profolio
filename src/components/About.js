20f2ada5d28150a10973c39c368073d1ac5327f8
Matt McCall <mmccall25@gmail.com>
2021-11-06 13:32:12 -0400

    Setup pages

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import react from 'react';
+
+
+function About(){
+    return(
+        <section id = "about">
+            <div>
+                Test
+            </div>
+        </section>
+    );
+}
+
+export default About;
---
+++ src/components/Projects.js
@@ -0,0 +1,14 @@
+import react from 'react';
+
+
+function Projects(){
+    return(
+        <section id = "projects">
+            <div>
+                Test
+            </div>
+        </section>
+    );
+}
+
+export default Projects;
