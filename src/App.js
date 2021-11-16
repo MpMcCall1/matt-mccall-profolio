55053def06138e37d0a1539407334b1af9b177a1
Matt McCall <mmccall25@gmail.com>
2021-11-16 17:47:04 -0500

    Laptop switch 0

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import About from './components/About.js';
 import Projects from './components/Projects.js';
+import Navbar  from './components/Navbar';
 
 function App() {
   return (
     <main >
+      <Navbar />
       <About />
       <Projects />
     </main>
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import react from 'react';
+
+function Navbar() {
+    return(
+        <section>
+            <ul>
+                <li><a herf="#">Home</a></li>
+                <li><a herf="#">About</a></li>
+                <li><a herf="#">Projects</a></li>
+            </ul>
+        </section>
+    ) 
+}
+
+export default Navbar;
